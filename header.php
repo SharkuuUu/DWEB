685980f05bafd3d8022ba1a4b8034c3a9ed354c0
Sharku <jensonlobo2122@gmail.com>
2025-12-03 01:49:36 +0800

    Update navigation links in header.php

--- header.php
+++ header.php
@@ -9,13 +9,12 @@
 <nav> 
     <ul>
         <li><a href="index.php">Home</a></li>
-        <li><a href="if.php">If Statement</a></li>
-        <li><a href="if_else_if.php">If Else If</a></li>
-        <li><a href="if_else.php">If Else Statement</a></li>
-        <li><a href="while_loop.php">While Loop</a></li>
-        <li><a href="do_while.php">Do While Loop</a></li>
-        <li><a href="foreach.php">For Each</a></li>
-        <li><a href="match.php">Match Expression</a></li>
+        <li><a href="OrderingSystem.php">Order</a></li>
+        <li><a href="If.php">If Statement</a></li>
+        <li><a href="IfElse.php">If Else Statement</a></li>
+        <li><a href="WhileLoop.php">While Loop</a></li>
+        <li><a href="ForEach.php">For Each</a></li>
+        <li><a href="Match.php">Match Expression</a></li>
         <li><a href="switch.php">Switch Statement</a></li>
     </ul>
 </nav>
